575a667f0457c58251a0f05c79665bbf7d265289
lijiye <lijiye@jingdaka.com>
2020-01-02 10:35:10 +0800

    算法

---
+++ leetcode/206. 反转链表.js
@@ -0,0 +1,32 @@
+// 反转一个单链表。
+//
+// 示例:
+//
+//   输入: 1->2->3->4->5->NULL
+// 输出: 5->4->3->2->1->NULL
+// 进阶:
+//   你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (!head || !head.next) return head;
+  let pre = null
+  let cur = head
+  while (cur) {
+    const next = cur.next
+    cur.next = pre
+    pre = cur
+    cur = next
+  }
+  return pre
+};
